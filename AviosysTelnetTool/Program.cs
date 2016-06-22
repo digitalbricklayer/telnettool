4bc84c0eaa09eeb7fd90943f64de5595916eeacf
Jack Hughes <jhughes@openxtra.co.uk>
2016-06-22 11:25:25 +0100

    Implements very simple Aviosys telnet client.

---
+++ AviosysTelnetTool/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using PrimS.Telnet;
+
+namespace AviosysTelnetTool
+{
+    /// <summary>
+    /// Entry point for the Aviosys telnet tool.
+    /// </summary>
+    class Program
+    {
+        private const int TimeoutMs = 100;
+
+        static void Main(string[] args)
+        {
+            using (Client client = new Client("10.0.0.81", 23, new System.Threading.CancellationToken()))
+            {
+                Debug.Assert(client.IsConnected);
+
+                string welcomeMessage = client.TerminatedRead("->", TimeSpan.FromMilliseconds(TimeoutMs));
+                Console.Out.WriteLine(welcomeMessage);
+                Debug.Assert(welcomeMessage.TrimEnd(' ').EndsWith("->"));
+
+                // Log into the unit using default user name and password
+                client.WriteLine("admin:12345678");
+                string loginResponse = client.TerminatedRead("->", TimeSpan.FromMilliseconds(TimeoutMs));
+                Console.Out.WriteLine(loginResponse);
+                Debug.Assert(loginResponse.TrimEnd(' ').EndsWith("->"));
+
+                // Send the help command
+                client.WriteLine("help");
+                string helpResponse = client.TerminatedRead("->", TimeSpan.FromMilliseconds(TimeoutMs));
+                Console.Out.WriteLine(helpResponse);
+            }
+
+            Console.Out.WriteLine("Press [Enter] to terminate.");
+            Console.In.ReadLine();
+        }
+    }
+}
